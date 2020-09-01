27ea48d3236c033f8e4cd5accceedce4c90742f5
Marcelo Mena <marcelomena010@gmail.com>
2020-09-01 00:09:39 -0300

    feature: Select field with generation filter

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
+import Select from 'react-select';
 
 import { Pokemon } from '../components';
 
@@ -8,20 +9,37 @@ import Dex from '../pokemons.json';
 
 import SearchIcon from '../public/images/icons/search.svg';
 
+const options = [
+    { value: 1, from: 0, to: 151, label: 'Generation I' },
+    { value: 2, from: 151, to: 251, label: 'Generation II' },
+    { value: 3, from: 251, to: 386, label: 'Generation III' },
+    { value: 4, from: 386, to: 493, label: 'Generation IV' },
+    { value: 5, from: 493, to: 649, label: 'Generation V' },
+    { value: 6, from: 649, to: 721, label: 'Generation VI' },
+    { value: 7, from: 721, to: 807, label: 'Generation VII' },
+];
+
 const Home = ({ initialDex, fullDex }) => {
-    const [search, setSearch] = useState('');
+    const [search, setSearch] = useState(false);
+    const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState(null);
+    const [selectedGen, setSelectedGen] = useState(null);
 
     const handleSearch = (e) => {
         const { value } = e.target;
-        setSearch(value);
+        setSearchValue(value);
         // setSearchResult(fullDex.filter((pokemon) => pokemon.name.toLowerCase().match(value)));
         setSearchResult(fullDex.filter((pokemon) => pokemon.name.toLowerCase().indexOf(value) !== -1));
+        setSearch(true);
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
+    useEffect(() => {
+        if (!selectedGen) {
+            return setSelectedGen(null);
+        }
+        setSearchResult(fullDex.slice(selectedGen.from, selectedGen.to));
+        setSearch(true);
+    }, [selectedGen]);
 
     return (
         <div className="container-index">
@@ -30,17 +48,25 @@ const Home = ({ initialDex, fullDex }) => {
             </Head>
 
             <main className="content">
-                <h1>NATIONAL DEX</h1>
+                <h1>{selectedGen ? selectedGen.label : 'NATIONAL DEX'}</h1>
 
-                <form onSubmit={handleSubmit} onChange={handleSearch}>
+                <section className="inputs">
                     <div className="search-box">
-                        <input type="text" placeholder="Search" />
-                        <img src={SearchIcon} alt="search" value={search} />
+                        <input type="text" placeholder="Search" value={searchValue} onChange={handleSearch} />
+                        <img src={SearchIcon} alt="search" />
                     </div>
-                </form>
 
-                <section>
-                    {search !== ''
+                    <Select
+                        options={options}
+                        id="select"
+                        instanceId="select"
+                        placeholder="Gen"
+                        onChange={setSelectedGen}
+                    />
+                </section>
+
+                <section className="pokemon-grid">
+                    {search
                         ? searchResult.map((pokemon) => (
                               <Pokemon key={pokemon.id} id={pokemon.id} name={pokemon.name} types={pokemon.typeList} />
                           ))
